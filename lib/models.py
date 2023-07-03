d582128ab9d25578e683c67941212d219e74efc8
Christian Garcia <bones192nd@gmail.com>
2023-07-03 11:46:18 -0400

    confusing lession

--- lib/models.py
+++ lib/models.py
@@ -10,6 +10,43 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+game_user = Table('game_users', 
+                  Base.metadata, 
+                  Column('game_id', ForeignKey('games.id'), primary_key=True),
+                  Column('user_id', ForeignKey('users.id'), primary_key=True),
+                  extend_existing = True,
+                  )
+
+# class GameUser(Base):
+#     __tablename__ = 'game_users'
+
+#     id = Column(Integer(), primary_key=True)
+#     game_id = Column(Integer(), ForeignKey('games.id'))
+#     user_id = Column(Integer(), ForeignKey('users.id'))
+
+#     game = relationship('User', back_populates='game_users')
+#     user = relationship('Game', back_populates='game_users')
+
+#     def __repr__(self):
+#         return f'GameUser(game=id={self.game_id}, user_id={self.user_id})'
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    create_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    games = relationship('Game', secondary=game_user, back_populates='users')
+    reviews = relationship('Review', backref=backref('user'), cascade='all, delete-orphan')
+    
+
+    def __repr__(self):
+        return f'User(id={self.id}, name={self.name})'
+
+
 class Game(Base):
     __tablename__ = 'games'
 
@@ -19,7 +56,11 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
-    reviews = relationship('Review', backref=backref('game'))
+    # for an association model, the following changes to 
+    # reviews = relationship('Review', back_population='game', cascade='all, delete-orphan')
+    # users = association_proxy('reviews', users, creator = lambda us: Review(user=us))
+    users = relationship('User', secondary=game_user, back_populates='games')
+    reviews = relationship('Review', backref=backref('game'), cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
@@ -34,6 +75,7 @@ class Review(Base):
     comment = Column(String())
     
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
